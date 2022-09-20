1672cfcc4adeb78b481b54e3c15d199125131af1
kamrankamrankhan <kamrankamrankhan825@gmail.com>
2022-09-20 20:19:47 +0500

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello, from kamran'))
+app.get('/', (req, res) => res.send('Hello, world 123'))
 
 var server = app.listen(process.env.PORT || 4000, () => {
     console.log("Listening on port " + server.address().port + "...");
