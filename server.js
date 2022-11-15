ac0585801137ef1532f1e8c19a9fe6b539cfbb17
kamran khan <91130565+kamrankamrankhan@users.noreply.github.com>
2022-11-16 02:12:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello, world 123'))
+app.get('/', (req, res) => res.send('Hello, kamran how are you doing?'))
 
 var server = app.listen(process.env.PORT || 4000, () => {
     console.log("Listening on port " + server.address().port + "...");
